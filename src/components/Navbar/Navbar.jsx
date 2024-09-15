2ecea68b98867f142ddad40a269695247756566d
NILESH KUMAR SHUKLA <nilesh.shukla47@gmail.com>
2024-09-15 18:59:51 +0530

    navbar-done

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,27 @@
 //import React from 'react'
+import { assets } from '../../assets/assets'
 import './Navbar.css'
 
 const Navbar = () => {
   return (
-    <div>
-      
+    <div className='Navbar'>
+      <img src={assets.logo} alt="" className="logo" />
+      <ul className='navbar-menu'>
+      <li>home</li>
+      <li>menu</li>
+      <li>mobile-app</li>
+      <li>contact us</li>
+      </ul>
+        <div className="navbar-right">
+        <img src={assets.search_icon} alt='' />
+        <div className="navbar-search-icon">
+          <img src={assets.basket_icon} alt='' />
+          <div className='dot'></div>
+        </div>
+        <button>Sign In</button>
+      </div>
     </div>
+    
   )
 }
 
